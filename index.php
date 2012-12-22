c5e53f199ff3c8b4b80b1591f25494933787fc09
Marek <marek@urmoshop.lt>
2012-12-22 14:52:03 +0200

    sukuriai css faila

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
+<html>
+<title>Bendras darbelis</title>
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+
 <?php
 echo date("Y-m-d");
+
+
+
+</body>
+
+</html>
